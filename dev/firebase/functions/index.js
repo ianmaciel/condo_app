05e1a11dbaf2b621a2d938edc2b2cd9673cfd10c
Ian Koerich Maciel <ianmaciel@gmail.com>
2022-06-24 14:19:55 -0300

    [functions] disable functions

--- dev/firebase/functions/index.js
+++ dev/firebase/functions/index.js
@@ -1,7 +1,6 @@
 
 
-const ewelink = require("./ewelink");
-const functions = require("firebase-functions");
-
-exports.getDevices = functions.https.onCall(ewelink.getDevices);
-exports.toggleCarGate = functions.https.onCall(ewelink.toggleCarGate);
+//const ewelink = require("./ewelink");
+//const functions = require("firebase-functions");
+//#exports.getDevices = functions.https.onCall(ewelink.getDevices);
+//exports.toggleCarGate = functions.https.onCall(ewelink.toggleCarGate);
